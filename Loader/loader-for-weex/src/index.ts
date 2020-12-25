d31ad823aaa55cd1c1c196a8cb4dc18a3389b3b0
zollero <corona7@163.com>
2020-12-25 13:41:24 +0800

    add vue loader for prism

---
+++ Loader/loader-for-weex/src/index.ts
@@ -0,0 +1,26 @@
+module.exports = function(src: string): string {
+  const tagWithClickHandler = /\<(\w|-)*([\w\=\#\(\)\"\&\.\-\'\`\|\[\]\{\}\+\,\s\?\:\!\_\/@$\u4e00-\u9fa5]*)((@click|v-on:click)="([\w\s\(\)\.\,\=\>\']*)")([\w\=\#\(\)\"\&\.\-\'\`\|\[\]\{\}\+\,\s\?\:\!\_\/@$\u4e00-\u9fa5]*)(\/)?\>/g;
+  const tagWithClick = /\<(\w|-)*([\w\=\#\(\)\"\&\.\-\'\`\|\[\]\{\}\+\,\s\?\:\!\_\/@$\u4e00-\u9fa5]*)((@click|v-on:click)="([\w\s\(\)\.\,\=\>\']*)")([\w\=\#\(\)\"\&\.\-\'\`\|\[\]\{\}\+\,\s\?\:\!\_\/@$\u4e00-\u9fa5]*)(\/)?\>/;
+
+  const tags = src.match(tagWithClickHandler);
+
+  if (tags) {
+    for (let i = 0; i < tags.length; i++) {
+      const clickRegexResult = tagWithClick.exec(tags[i])
+      if (clickRegexResult) {
+        const startIndex = tags[i].indexOf(clickRegexResult[3]);
+        if (startIndex !== -1) {
+          let uniqueId = `prismFunctionName="${clickRegexResult[5]}" `;
+          const classRegex = /(:class|v-on:class|class)="([\w\=\-\'\[\]\`\,\s\?\:]*)"/
+          const classExecResult = classRegex.exec(tags[i]);
+          if (classExecResult) {
+            uniqueId += `prismClassName="${classExecResult[2]}" `
+          }
+          const replaceStr = tags[i].substring(0, startIndex) + uniqueId + tags[i].substring(startIndex)
+          src = src.replace(tags[i], replaceStr);
+        }
+      }
+    }
+  }
+  return src
+}
