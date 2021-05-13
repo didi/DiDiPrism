1f629a724f95dbda07f870993e3df09935881384
haoliangjian <haoliangjian@tal.com>
2021-05-13 18:27:13 +0800

    fix : 特殊场景下的获取视图的控制器数组
    “viewController.view被添加到了目标VC.view上，但是viewController没有被添加到目标VC上。”
    该场景下修复递归调用问题

--- iOS/DiDiPrism/Src/Core/Instruction/Util/PrismInstructionResponseChainUtil.m
+++ iOS/DiDiPrism/Src/Core/Instruction/Util/PrismInstructionResponseChainUtil.m
@@ -53,7 +53,7 @@
         }
         //viewController.view被添加到了目标VC.view上，但是viewController没有被添加到目标VC上。
         if (viewController.view.superview.prism_viewController) {
-            NSArray<UIViewController*> *moreVC = [self getParentViewControllersOfView:viewController.view];
+            NSArray<UIViewController*> *moreVC = [self getParentViewControllersOfView:viewController.view.superview];
             if (moreVC.count) {
                [viewControllers insertObjects:moreVC atIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0,[moreVC count])]];
             }
