69b8756d22c02212c6badd159ebdae35a89495f6
Hulk <ronghao@didichuxing.com>
2025-04-07 12:15:40 +0800

    [feat]iOS事件分发增加多线程保护。

--- iOS/DiDiPrism/Src/Core/EventDispatcher/PrismEventDispatcher.m
+++ iOS/DiDiPrism/Src/Core/EventDispatcher/PrismEventDispatcher.m
@@ -9,6 +9,7 @@
 
 @interface PrismEventDispatcher()
 @property (nonatomic, strong) NSMutableArray<id<PrismDispatchListenerProtocol>> *allListeners;
+@property (nonatomic, strong) dispatch_queue_t syncQueue;
 @end
 
 @implementation PrismEventDispatcher
@@ -22,28 +23,46 @@
     return dispatcher;
 }
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _syncQueue = dispatch_queue_create("com.prism.eventdispatcher.queue", DISPATCH_QUEUE_SERIAL);
+    }
+    return self;
+}
+
 #pragma mark - public method
 - (void)registerListener:(id<PrismDispatchListenerProtocol>)listener {
     if (!listener) {
         return;
     }
-    if (![self.allListeners containsObject:listener]) {
-        [self.allListeners addObject:listener];
-    }
+    dispatch_sync(self.syncQueue, ^{
+        if (![self.allListeners containsObject:listener]) {
+            [self.allListeners addObject:listener];
+        }
+    });
 }
 
 - (void)unregisterListener:(id<PrismDispatchListenerProtocol>)listener {
     if (!listener) {
         return;
     }
-    [self.allListeners removeObject:listener];
+    dispatch_sync(self.syncQueue, ^{
+        [self.allListeners removeObject:listener];
+    });
 }
 
 - (void)dispatchEvent:(PrismDispatchEvent)event withSender:(NSObject*)sender params:(nullable NSDictionary*)params {
-    NSArray<id<PrismDispatchListenerProtocol>> *listeners = [self.allListeners copy];
-    for (id<PrismDispatchListenerProtocol> listener in listeners) {
-        [listener dispatchEvent:event withSender:sender params:params];
-    }
+    __block NSArray<id<PrismDispatchListenerProtocol>> *listenersCopy = nil;
+    dispatch_sync(self.syncQueue, ^{
+        listenersCopy = [self.allListeners copy];
+    });
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        for (id<PrismDispatchListenerProtocol> listener in listenersCopy) {
+            [listener dispatchEvent:event withSender:sender params:params];
+        }
+    });
 }
 
 #pragma mark - private method
